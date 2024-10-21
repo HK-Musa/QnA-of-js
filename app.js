bd170cb0c3e37a5486e2e24beb4c3e0634339bf1
HK-Musa <khatoonhajira71@gmail.com>
2024-10-21 19:24:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,259 @@
+
+//********Chapter 1**  Assignment 5 *****   Task 1 **********
+
+//alert("Welcome to My Web Site");
+
+//********Chapter 1**   Assignment 5 *****   Task 2 **********
+
+// alert("Error! Please Enter a valid password");
+
+//********Chapter 1**   Assignment 5 *****   Task 3 **********
+
+// alert("Welcome to JS Land \n Happy Coding!");
+
+//********Chapter 1**   Assignment 5 *****   Task 4 **********
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+//********Chapter 1**   Assignment 5 *****   Task 5 **********
+
+// alert("Hello.. I Can run Js through my browser's console"); // this is also done in chrome developer mode
+
+
+
+//********Chapter 2**   Assignment 5 *****   Task 1 **********
+
+// var UserName="Abid";
+// alert(UserName);
+
+
+//********Chapter 2**   Assignment 5 *****   Task 2 **********
+
+// var myName="Muhammad Abid";
+// alert(myName);
+
+//********Chapter 2**  Assignment 5 *****   Task 3 **********
+
+// var message="Hello World";
+// alert(message);
+
+//********Chapter 2**  Assignment 5 *****   Task 4 **********
+
+// var studentBio1=prompt("","Enter Your Name");
+// var studentBio2=prompt("","Enter Your Age");
+// var studentBio3=prompt("","Enter Your Qualification");
+// alert(studentBio1);
+// alert(studentBio2 + " years old");
+// alert(studentBio3);
+
+//********Chapter 2**     Assignment 5 *****   Task 5 **********
+
+// var a="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(a);
+
+//********Chapter 2**  Assignment 5 *****   Task 6 **********
+
+// var email="abidkhan11845@gmail.com";
+// alert("My email address is " + email);
+
+//********Chapter 2**   Assignment 5 *****   Task 7 **********
+
+// var book="A smarter way to Learn JavaScript";
+// alert("I am  trying to Learn from the Book "+book);
+
+//********Chapter 2**  Assignment 5 *****   Task 8 **********
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//********Chapter 2**  Assignment 5 *****   Task 9 **********
+
+// var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a);
+
+//********Chapter 3**  Assignment 5 *****   Task 1 **********
+
+// var age=19;
+// alert("I am "+age+" years old");
+
+//********Chapter 3**    Assignment 5 *****   Task 2 **********
+
+//Don't Know how tp solve
+
+//********Chapter 3**   Assignment 5 *****   Task 3 **********
+
+// var birthYear=1998;
+// alert("My birth year is "+birthYear+"\n Data type of my declared variable is number");
+
+//********Chapter 3**     Assignment 5 *****   Task 4 **********
+
+// var visitorName=prompt("","Enter Your Name");
+// var productTitle=prompt("","Enter Product Name");
+// var quanTity=prompt("","Enter Quantity Name");
+// document.write(visitorName+" ordered "+quanTity+" "+productTitle+"(s) on Daraz Store");
+                                /////////// document.write(visitorName+" ordered"+quanTity+" "+productTitle"(s) on Daraz Store");  // here mistake is after producttitle "+"  sign not present
+
+
+//********Chapter 4**  Assignment 5 *****   Task 1 **********
+
+// var a=b=c="Null";
+// alert(a);
+// alert(b);
+// alert(c);
+
+//********Chapter 4**   Assignment 5 *****   Task 2 **********
+
+// var a=null,b=null,c=null,d=null,e;
+// // var a=b=c=d=e=null;
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// var null,alert,Nan,var,1,2,3; ////illegal variables
+
+
+//********Chapter 4**  Assignment 5 *****   Task 3 **********
+
+// document.write("<h1>Rules for naming JS variables</h1><br>Variable names can only contain number , $ and ______. For example : $my_1stVariable <br> Variables must begin with a letters , $ or _____. For example $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords");
+
+//********Chapter 5**   Assignment 5 *****   Task 1 **********
+
+// var a=+prompt("","Enter First Value");
+// var b=+prompt("","Enter Second Value");
+// var c=a+b;
+// document.write("Sum of "+a+" and "+b+" is "+c+".");
+
+
+//********Chapter 5**     Assignment 5 *****   Task 2 **********
+
+// var a=+prompt("","Enter First Value");
+// var operator=prompt("","Enter Operator to Perform")
+// var b=+prompt("","Enter Second Value");
+// if(operator === "-")
+//     {
+//         var c=a-b;
+//         document.write("Difference of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "*")
+//     {
+//         var c=a*b;
+//         document.write("Multiple of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "/")
+//     {
+//         var c=a/b;
+//         document.write("Division of "+a+" by "+b+" is "+c+".");
+//     }
+// else if(operator === "+")
+//     {
+//         var c=a+b;
+//         document.write("Sum of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "%")
+//     {
+//         var c=b/a;
+//         document.write("Modulus of "+a+" and "+b+" is "+c*100+"% .");
+//     }
+
+//********Chapter 5**   Assignment 5 *****   Task 3 **********
+
+// var a;
+// document.write("Value after variable declaration is : "+a);
+// a=5;
+// document.write("<br>Initial Value is : "+a);
+// a++;
+// document.write("<br>Value after increment of '1' is : "+a);
+// a=a+7;
+// document.write("<br>Value after addition of '7' is : "+a);
+// a--;
+// document.write("<br>Value after decrement is : "+a);
+// a=a/3;
+// document.write("<br>The remainder is : "+a)
+
+//********Chapter 5**  Assignment 5 *****   Task 4 **********
+
+// var ticketPrice=600;
+// var buy=prompt("","Enter Quantity of Tickets");
+// document.write("Total cost to buy "+buy+" tickets to a movie is "+(ticketPrice*buy)+"PKR")
+
+//********Chapter 5**  Assignment 5 *****   Task 5 **********
+
+// var taBle=prompt("","Enter Value to Generate Table");
+// document.write("Table of "+taBle);
+// for(i=1;i<=10;i++)
+//     {
+//         document.write("<br>"+taBle+" * "+i+" = "+(i*taBle));
+
+//     }
+
+//********Chapter 5**  Assignment 5 *****   Task 6 **********
+
+// var celTemp=prompt("","Enter Celsius Temperature");
+// var fahTemp=prompt("","Enter Fahrenheit Temperature");
+// var fahconv=(celTemp*(9/5))+32;
+// var celconv=(fahTemp-32)*(5/9);
+// document.write(celTemp+"*C is "+fahconv+"*F<br>");
+// document.write(fahTemp+"*F is "+celconv+"*C");
+
+//********Chapter 5**  Assignment 5 *****   Task 7 **********
+
+// var priceItem1=prompt("","Price of item 1");
+// var quanItem1=prompt("","Quantity of item 1");
+// var priceItem2=prompt("","Price of item 2");
+// var quanItem2=prompt("","Quantity of item 2");
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("Price of item '1' is "+priceItem1+"<br>Quantity of item '1' is "+quanItem1+"<br>Price of item '2' is "+priceItem2+"<br>Quantity of item is '2' "+quanItem2+"<br>Shipping Charges 100<br>");
+// var cost=(priceItem1*quanItem1)+(priceItem2*quanItem2)+100;
+// document.write("<br>Total cost of your order is "+cost);
+
+//********Chapter 5**  Assignment 5 *****   Task 8 **********
+
+// var sub1=+prompt("","Enter Obained Marks Subject 1");
+// var sub2=+prompt("","Enter Obained Marks Subject 2");
+// totalMarks=980;
+// document.write("Total marks: "+totalMarks+"<br>Marks obtained: "+(sub1+sub2)+"<br>Percentage:"+((sub1+sub2)/totalMarks)*100+"%");
+
+//********Chapter 5**  Assignment 5 *****   Task 9 **********
+
+// var dollar=+prompt("","Enter Quantity of Dollar");
+// var saudi=+prompt("","Enter Quantity of saudi");
+// document.write("Total Currency in PKR: "+((104.80*dollar)+(saudi*28)))
+
+// ////////        oR
+// document.write("<br>Total Currency in PKR: "+((104.80*10)+(25*28)))
+
+//********Chapter 5**  Assignment 5 *****   Task 10 **********
+
+// var val1=+prompt("","Enter 1st value");
+// val1=val1+5;
+// document.write("<br>After Adding 5 is "+val1);
+// val1=val1*10;
+// document.write("<br>After Multiplying by 10 is "+val1);
+// val1=val1/2;
+// document.write("<br>After Dividing by 2 is "+val1);
+
+            ////////  OR
+
+// var val1=+prompt("","Enter 1st value");
+// document.write("After Adding 5 is "+(val1+5)+"<br>After Multiplying by 10 is "+((val1+5)*10)+"<br>After Dividing by 2 is "+((val1+5)*10)/2);
+
+//********Chapter 5**  Assignment 5 *****   Task 11 **********
+
+// var currentYear=prompt("","Enter Current Year! ");
+// var birthYear=prompt("","Enter Birth Year! ");
+// document.write("Current Year: "+currentYear+"<br>Birth Year: "+birthYear+"<br>Your Age is: "+(currentYear-birthYear));
+
+//********Chapter 5**  Assignment 5 *****   Task 12 **********
+
+// var raDius=prompt("","Enter value of Radius");
+// document.write("<h1>The Geometrizer</h1>Radius of a circle: "+raDius+"<br>The circumference is: "+(raDius*2*3.142)+"<br>The area is: "+(raDius*raDius*3.142));
+
+//********Chapter 5**  Assignment 5 *****   Task 13 **********
+
+// var favSnack=prompt("","Your Favourite Snack! ");
+// var currAge=prompt("","Your Current Age ");
+// var maxAge=prompt("","Your Estimated Maximun Age ");
+// var perDaySnack=prompt("","Eat Per day Snacks ");
+// document.write("<h1>The Lifetime Supply Calculator</h1>Favourite Snack: "+favSnack+"<br>Current Age"+currAge+"<br>Estimated Maximum Age: "+maxAge+"<br>Amount of snacks per day: "+perDaySnack+"<br>You will need "+(maxAge-currAge)*perDaySnack+" chocolate chip to last you until the ripe old age of 65");
+
